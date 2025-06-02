74dd9a3814e428c5cefc8af08920a3ef425c13bb
Nada Zirari <zirarinada20@gmail.com>
2025-06-02 14:57:53 +0100

    add file env

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\ProductController;
-
-Route::get('/products', [ProductController::class, 'index']);
